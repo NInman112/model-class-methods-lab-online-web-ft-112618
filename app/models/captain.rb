cd1735c1bccb1d827d06a91daf62f236efa080e1
Nick Inman <nick@theoatmeal.com>
2019-02-17 02:15:59 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -10,7 +10,7 @@ class Captain < ActiveRecord::Base
   end
 
   def self.talented_seafarers
-    all.includes(boats: :classifications).where(classifications: { name: "Motorboat", name: "Sailboat" }).uniq & where("name in (?)", self.sailors.pluck(:name)
+    all.includes(boats: :classifications).where(classifications: { name: "Motorboat", name: "Sailboat" }).uniq
   end
 
   def self.non_sailors
