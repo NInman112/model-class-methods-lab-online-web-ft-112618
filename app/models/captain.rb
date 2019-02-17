000915f373a9fcc54e9d841560f1a557232306f7
Nick Inman <nick@theoatmeal.com>
2019-02-17 01:22:10 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,19 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    all.includes(boats: :classifications).where(classifications: { name: "Catamaran" })
+  end
+
+  def self.sailors
+    all.includes(boats: :classifications).where(classifications: { name: "Sailboat" }).uniq
+  end
+
+  def self.talented_seafarers
+all.includes(boats: :classifications).where(classifications: { name: "Motorboat", name: "Sailboat" })
+  end
+
+  def self.non_sailors
+
+  end
 end
